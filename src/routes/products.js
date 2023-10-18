5fcf7b4bff4bbd632954af7e3a298009111823f7
facumoyano <facumoyano44@gmail.com>
2023-10-18 14:51:34 -0300

    refactor MVC

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,107 +1,14 @@
 import { Router } from "express";
-import ProductManager from "../dao/services/product/productDBService.js";
-import Product from "../dao/models/productModel.js";
+import ProductController from "../dao/controllers/productController.js";
 
 const router = Router();
+const controller = new ProductController();
 
-router.get("/", async (req, res) => {
-  const manager = new ProductManager();
-  try {
-    const { page = 1, limit = 10, sort, category, available, title } = req.query;
-    const manager = new ProductManager();
-    const products = await manager.getProducts(page, limit, sort, category, available, 'products', title );
-    res.send(products);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error to get the Products");
-  }
-});
-
-router.get("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const manager = new ProductManager();
-  try {
-    const product = await manager.getProductById(productId);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send("Product not found");
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error to get the Product");
-  }
-});
-
-router.post("/", async (req, res) => {
-  const product = req.body;
-  if (
-    !product.title ||
-    !product.description ||
-    !product.price ||
-    !product.code ||
-    !product.stock ||
-    !product.category
-  ) {
-    res.status(400).send("Fields are missing");
-    return;
-  }
-  const newProduct = new Product({
-    title: product.title,
-    description: product.description,
-    price: product.price,
-    code: product.code,
-    stock: product.stock,
-    category: product.category,
-    status: true,
-  });
-  try {
-    const savedProduct = await newProduct.save();
-    res.status(201).send(savedProduct);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error to save the Product");
-  }
-});
-
-router.put("/:pid", async (req, res) => {
-  const product = req.body;
-  if (
-    !product.title ||
-    !product.description ||
-    !product.price ||
-    !product.code ||
-    !product.category
-  ) {
-    res.status(400).send("Fields are missing");
-    return;
-  }
-  const productId = req.params.pid;
-  const manager = new ProductManager();
-  try {
-    const updatedProduct = await manager.updateProduct(productId, product);
-    if (updatedProduct) {
-      res.send(updatedProduct);
-    } else {
-      res.status(404).send("Product not found");
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error to update the Product");
-  }
-});
-
-router.delete("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const manager = new ProductManager();
-  try {
-    const message = await manager.deleteProduct(productId);
-    res.send(message);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error to delete the Product");
-  }
-});
+router.get("/", controller.getProducts);
+router.get("/:pid", controller.getProductById);
+router.post("/", controller.addProduct);
+router.put("/:pid", controller.editProductById);
+router.delete("/:pid", controller.deleteProductById);
 
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,31 +1,16 @@
 import {Router} from 'express';
-import UserService from '../dao/services/user/userService.js';
+import UserController from '../dao/controllers/userControler.js';
 import passport from 'passport';
 
 
-const US = new UserService();
+const controller = new UserController();
 const router = Router();
 
-router.post("/register", passport.authenticate('register',{failureRedirect: '/register'}), async (req, res) => {
-    try {
-        req.session.registerSuccess = true;
-        res.redirect("/login");
-    } catch (error) {
-        req.session.registerFailed = true;
-        res.redirect("/register");
-        console.error(error)
-    }
-});
+router.post("/register", passport.authenticate('register',{failureRedirect: '/register'}), controller.registerUser);
 
-router.post("/login", passport.authenticate('login', {failureRedirect: '/login'}), (req, res) => {
-    req.session.user = req.user;
-    res.redirect("/");
-});
+router.post("/login", passport.authenticate('login', {failureRedirect: '/login'}), controller.loginUser);
 
-router.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.redirect("/login");
-});
+router.get("/logout", controller.logoutUser);
 
 
 export default router;
